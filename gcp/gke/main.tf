296adf750fe7d671d9fe0613e7d7c30459ec6e78
barchitect0 <tbng84@gmail.com>
2024-11-22 10:01:59 +0700

    refactor

--- gcp/gke/main.tf
+++ gcp/gke/main.tf
@@ -14,6 +14,6 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 4
 
   # Ensure deletion protection is set to false
-  #deletion_protection = false
+  deletion_protection = false
 }
      
