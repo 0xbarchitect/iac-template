24e852e56057e3ba1acc701e7e762dfa657aef26
barchitect0 <tbng84@gmail.com>
2024-11-26 16:44:35 +0700

    gke for validator nodes

---
+++ nodes/gke/main.tf
@@ -0,0 +1,19 @@
+provider "google" {
+  project     = "nft2-440103"
+  region      = "us-central1"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "gke-cluster-20241115"
+  location = "us-central1"
+
+  node_config {
+    machine_type = "n1-standard-4"
+  }
+
+  initial_node_count = 4
+
+  # Ensure deletion protection is set to false
+  deletion_protection = false
+}
+     
