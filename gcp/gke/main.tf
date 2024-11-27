d50086733ee4746d4f1703761149203d49410b9c
barchitect0 <tbng84@gmail.com>
2024-11-27 11:53:29 +0700

    resource

--- gcp/gke/main.tf
+++ gcp/gke/main.tf
@@ -3,9 +3,9 @@ provider "google" {
   region      = var.GOOGLE_REGION
 }
 
-# resource "google_container_cluster" "primary" {
-#   name     = var.cluster_name
-#   location = var.GOOGLE_REGION
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.GOOGLE_REGION
 
 #   node_config {
 #     machine_type = var.instance_type
