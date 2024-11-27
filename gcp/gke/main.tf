796fc3907b22b210bb83de69404062c84949a54f
barchitect0 <tbng84@gmail.com>
2024-11-27 11:53:41 +0700

    node config

--- gcp/gke/main.tf
+++ gcp/gke/main.tf
@@ -7,9 +7,9 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.GOOGLE_REGION
 
-#   node_config {
-#     machine_type = var.instance_type
-#   }
+  node_config {
+    machine_type = var.instance_type
+  }
 
 #   initial_node_count = var.instance_number
 
