29320ac895459379bcc8cd19c5f9a26989004657
barchitect0 <tbng84@gmail.com>
2024-11-27 11:55:06 +0700

    resource

--- nodes/gke/main.tf
+++ nodes/gke/main.tf
@@ -3,9 +3,9 @@ provider "google" {
   region      = "us-central1"
 }
 
-# resource "google_container_cluster" "primary" {
-#   name     = "gke-cluster-20241115"
-#   location = "us-central1"
+resource "google_container_cluster" "primary" {
+  name     = "gke-cluster-20241115"
+  location = "us-central1"
 
 #   node_config {
 #     machine_type = "n1-standard-4"
