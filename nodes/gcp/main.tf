5d55f077986565c61568660ea2318035df8f75f7
barchitect0 <tbng84@gmail.com>
2024-11-29 14:44:40 +0700

    nodes testnet

---
+++ nodes/gcp/main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
+
+resource "google_compute_instance" "default" {
+  count        = var.instance_number
+  name         = "${var.cluster_name}-${count.index}"
+  machine_type = var.instance_type
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+}
---
+++ nodes/gcp/variables.tf
@@ -0,0 +1,26 @@
+variable "GOOGLE_CREDENTIALS" {
+  type = string
+}
+
+variable "GOOGLE_PROJECT" {
+  type = string
+}
+
+variable "GOOGLE_REGION" {
+  type = string
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the GKE cluster"  
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The type of the GKE node instance"  
+}
+
+variable "instance_number" {
+  type        = number
+  description = "The number of the GKE node instance"  
+}
