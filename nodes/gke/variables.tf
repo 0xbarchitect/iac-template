6a9a1c957abf19f17a3eaf581761f80be701c635
barchitect0 <tbng84@gmail.com>
2024-11-29 11:02:23 +0700

    project

--- nodes/gke/variables.tf
+++ nodes/gke/variables.tf
@@ -2,9 +2,9 @@ variable "GOOGLE_CREDENTIALS" {
   type = string
 }
 
-# variable "GOOGLE_PROJECT" {
-#   type = string
-# }
+variable "GOOGLE_PROJECT" {
+  type = string
+}
 
 # variable "GOOGLE_REGION" {
 #   type = string
