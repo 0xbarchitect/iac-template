325df2a3b3ae9837d3ed3c97b82bcf50294a13d4
barchitect0 <tbng84@gmail.com>
2024-12-02 15:39:02 +0700

    refactor

--- nodes/gcp/main.tf
+++ nodes/gcp/main.tf
@@ -1,48 +1,48 @@
-provider "google" {
-  project = var.GOOGLE_PROJECT
-  region  = var.GOOGLE_REGION
-}
+# provider "google" {
+#   project = var.GOOGLE_PROJECT
+#   region  = var.GOOGLE_REGION
+# }
 
-resource "google_compute_address" "static" {
-  count  = var.instance_number
-  name   = "${var.cluster_name}-static-ip-${count.index}"
-  region = var.GOOGLE_REGION
-}
+# resource "google_compute_address" "static" {
+#   count  = var.instance_number
+#   name   = "${var.cluster_name}-static-ip-${count.index}"
+#   region = var.GOOGLE_REGION
+# }
 
-resource "google_compute_instance" "default" {
-  count        = var.instance_number
-  name         = "${var.cluster_name}-${count.index}"
-  machine_type = var.instance_type
-  zone         = var.GOOGLE_ZONE
+# resource "google_compute_instance" "default" {
+#   count        = var.instance_number
+#   name         = "${var.cluster_name}-${count.index}"
+#   machine_type = var.instance_type
+#   zone         = var.GOOGLE_ZONE
 
-  tags = ["validator-nodes-firewall"]
+#   tags = ["validator-nodes-firewall"]
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
-      size  = 2048
-      type  = "pd-ssd"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-os-cloud/ubuntu-2004-lts"
+#       size  = 2048
+#       type  = "pd-ssd"
+#     }
+#   }
 
-  network_interface {
-    network = "default"
+#   network_interface {
+#     network = "default"
 
-    access_config {
-      nat_ip = google_compute_address.static[count.index].address
-    }
-  }
-}
+#     access_config {
+#       nat_ip = google_compute_address.static[count.index].address
+#     }
+#   }
+# }
 
-resource "google_compute_firewall" "default" {
-  name    = "validator-nodes-firewall"
-  network = "default"
+# resource "google_compute_firewall" "default" {
+#   name    = "validator-nodes-firewall"
+#   network = "default"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "9651"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22", "9651"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["validator-nodes-firewall"]
-}
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["validator-nodes-firewall"]
+# }
